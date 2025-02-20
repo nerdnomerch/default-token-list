73d909642d6bdab9bddc605029d3f3a5fc2593ad
NERD777 <137885674+nerdnomerch@users.noreply.github.com>
2025-02-20 14:16:40 +0700

    Update buildList.js

--- src/buildList.js
+++ src/buildList.js
@@ -3,51 +3,19 @@ require('dotenv').config()
 const arbitrumSepolia = require("./tokens/arbitrum-sepolia.json");
 const arbitrumOne = require("./tokens/arbitrum-one.json");
 const alephZero = require("./tokens/aleph-zero.json");
-const apechain = require("./tokens/apechain.json");
-const conwai = require("./tokens/conwai.json");
-const corn = require("./tokens/corn.json");
-const degen = require("./tokens/degen.json");
-const duckchain = require("./tokens/duckchain.json");
-const educhain = require("./tokens/educhain.json");
-const geist = require("./tokens/geist.json");
-const game7 = require("./tokens/game7.json");
 const gravity = require("./tokens/gravity.json");
 const molten = require("./tokens/molten.json");
-const plume = require("./tokens/plume.json");
 const proofOfPlay = require("./tokens/proof-of-play.json");
 const rari = require("./tokens/rari.json");
 const reya = require("./tokens/reya.json");
 const sanko = require("./tokens/sanko.json");
-const superposition = require("./tokens/superposition.json");
 const winr = require("./tokens/winr.json");
 const xai = require("./tokens/xai.json");
 
 const BASE_URL = process.env.BASE_URL || "https://token-list.camelot.exchange"
 
 module.exports = function buildList() {
-  const tokens = [
-    ...arbitrumSepolia,
-    ...arbitrumOne,
-    ...alephZero,
-    ...apechain,
-    ...conwai,
-    ...corn,
-    ...degen,
-    ...duckchain,
-    ...educhain,
-    ...game7,
-    ...geist,
-    ...gravity,
-    ...molten,
-    ...plume,
-    ...proofOfPlay,
-    ...rari,
-    ...reya,
-    ...sanko,
-    ...superposition,
-    ...winr,
-    ...xai
-  ]
+  const tokens = [...arbitrumSepolia, ...arbitrumOne, ...alephZero, ...gravity, ...molten, ...proofOfPlay, ...rari, ...reya, ...sanko, ...winr, ...xai]
   const processedTokens = JSON.parse(JSON.stringify(tokens).replace(/BASE_URL/g, BASE_URL))
   const parsed = version.split(".");
 
